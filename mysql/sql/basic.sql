73b52a268a03614c025e1174b5057ec6560edd47
minsoo822@naver.com <minsoo822@naver.com>
2022-07-28 14:15:35 +0900

    sql

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,2 +1,21 @@
 -- baebae database를 사용하겠다
-use baebae;
+USE baebae;
+
+-- 전체 컬럼 조회
+SELECT * FROM member2;
+
+-- 컬럼추가
+ALTER TABLE member2 ADD COLUMN age varchar(45);
+
+-- AFTER () 는 그 다음에 테이블을 만들어준다.
+
+ALTER TABLE member2 ADD COLUMN dob varchar(45) AFTER name;
+
+
+-- 컬럼 변경 
+ALTER TABLE member2 MODIFY COLUMN nickname varchar(100);
+
+-- 컬럼 이름변경
+ALTER
+
+
