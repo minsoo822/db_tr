c5552b26a05a32cc3f22e76fe1f81c8704913ca7
minsoo822@naver.com <minsoo822@naver.com>
2022-08-16 15:29:18 +0900

    sql

--- mysql/sql/basic_select.sql
+++ mysql/sql/basic_select.sql
@@ -23,29 +23,39 @@ inner join actor_award b on b.member_seq = a.seq
 -- join actor_award b on b.member_seq = a.seq
 ;
 
-
 -- 로그인
+SELECT
+	a.id
+    ,a.password
+FROM member a
+;
 
 -- 메인.오디션정보
-SELECT
-	name
-    ,producer
-    ,casting
-    ,age
-from post_odition
-WHERE 1=1
-	AND seq = 13
-    AND seq = 13
-    AND seq = 13
-    AND seq = 13
+SELECT 
+	a.name
+    ,a.producer
+    ,a.casting
+    ,a.age
+    ,a.gender
     
+FROM post_odition a
+order by
+	a.seq desc
+limit 0,6
 ;
 
 -- 메인.배우프로필
 SELECT 
+	
 	a.name
     ,a.age
 FROM member a
+WHERE 1=1
+	and a.user_type = 26
+order by
+-- a.seq asc
+	a.seq desc
+	limit 0,10
 ;	
 
 -- 메인.투어정보
@@ -54,10 +64,65 @@ SELECT
     ,a.type
     ,a.name
     ,a.address
-FROM tour_post a    
-
--- 투어정보목록
-
+    ,a.date
+    ,a.note
+FROM post_tour a    
+limit 0,9
+;
 
 -- 오디션정보목록
+SELECT 
+	a.name
+    ,a.producer
+    ,a.casting
+    ,a.age
+    ,a.gender
+FROM post_odition a
+	limit 0,6
+;
 
+-- 투어정보목록
+SELECT
+	a.seq
+    ,a.type
+    ,a.name
+    ,a.address
+    ,a.date
+    ,a.note
+FROM post_tour a    
+limit 0,9
+;
+
+-- 프로필 보기 남자
+SELECT
+a.user_type
+	,a.name
+    ,a.age
+-- (select  FROM )
+FROM member a
+	WHERE 1=1
+		and user_type = 26
+		and gender = 28
+;
+
+-- 배우 프로필 보기 여자
+SELECT 
+	a.name
+    ,a.age
+	
+FROM member a
+	WHERE 1=1
+		and user_type = 26
+        and gender = 29
+;
+
+-- 디렉터 보기
+SELECT 
+	a.name
+    ,a.director_type
+
+FROM member a    
+WHERE 1=1
+	and user_type = 27
+;
+    
